a332fc701ee458005755682b1776b2806100203f
Solomon Ajayi <temmyjay001@gmail.com>
2020-06-08 18:19:04 +0100

    Remove.js: worked with model

--- controllers/files.js
+++ controllers/files.js
@@ -1,7 +1,7 @@
-
+const File = require("../models/file.model");
 /**
  * Controllers for :
- * 
+ *
  * getFiles
  * getFile,
  * createFile,
@@ -9,8 +9,20 @@
  * deleteFile
  */
 class FileContoller {
-     
-
+  deleteFile(req, res) {
+    File.deleteOne({ _id: req.params.id })
+      .then(() => {
+        res.status(200).json({
+          status: true,
+          message: "File Deleted",
+        });
+      })
+      .catch((error) => {
+        res.status(400).json({
+          status: error,
+        });
+      });
+  }
 }
 
 module.exports = new FileContoller();
