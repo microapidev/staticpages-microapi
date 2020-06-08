4391e89919f462ef9f203f160505a4dbfd6838f1
saucecodee <ashawinta@gmail.com>
2020-06-08 21:12:53 +0100

    fix: file controller bugs

--- middleware/error.js
+++ middleware/error.js
@@ -3,7 +3,7 @@ const response = require("../utils/response")
 
 const errors = (error, req, res, next) => {
      if (error instanceof CustomError) {
-          res.status(error.status).send(response(error.message, null, false));
+          res.status(error.status).json(response(error.message, null, false));
      } else {
           res.status(500).send(response("Sorry something happened", null, false));
      }
