083711463f7a67140a3ccf5385d1d815cdaf5b72
Alao-Abiodun <abiodunDev@gmail.com>
2020-06-09 10:05:42 +0100

    no-changes-made

--- controllers/files.js
+++ controllers/files.js
@@ -13,68 +13,68 @@ const CustomError = require("./../utils/CustomError");
  */
 
 class FileContoller {
-  // Add file
-  async createFile(req,res){
-    // Add file path to request body
-    if (req.file) req.body["fileURL"] = req.file.path
+    // Add file
+    async createFile(req, res) {
+        // Add file path to request body
+        if (req.file) req.body["fileURL"] = req.file.path
 
-    let file = new File(req.body)
-    file.save()
+        let file = new File(req.body)
+        file.save()
 
-    res.status(201).json(response("File created", file, true))
-  }
+        res.status(201).json(response("File created", file, true))
+    }
 
-  // Get one file
-  async getFile(req, res) {
-    await File.findOne({ _id: req.params.fileId }, (err, file) => {
-      if (err) throw new CustomError("Error occured while retriving files");
+    // Get one file
+    async getFile(req, res) {
+        await File.findOne({ _id: req.params.fileId }, (err, file) => {
+            if (err) throw new CustomError("Error occured while retriving files");
 
-      if (!file)
-        return res.status(404).json(response("File Not Found", null, false));
+            if (!file)
+                return res.status(404).json(response("File Not Found", null, false));
 
-      res.status(200).json(response("File Found", file, true))
-    })
-  }
+            res.status(200).json(response("File Found", file, true))
+        })
+    }
 
-  //route handler to get all files
-  async getFiles(req, res) {
-    let files = await File.find({});
+    //route handler to get all files
+    async getFiles(req, res) {
+        let files = await File.find({});
 
-    if (!files)
-      return res.status(200).json(response("No Files Found", files, true));
+        if (!files)
+            return res.status(200).json(response("No Files Found", files, true));
 
-    return res.status(200).json(response("All Files Found", files, true));
-  }
-
-  
-  async deleteFile(req, res) {
-    const file = await File.deleteOne({ _id: req.params.id }, (err, file) => {
-      if (err) throw new CustomError("Error occured while deleting file");
-      if (!file)
-        return res.status(404).json(response("File Not Found", null, false));
-
-      res.status(200).json(response("File Deleted", null, true));
-    });
-  }
+        return res.status(200).json(response("All Files Found", files, true));
+    }
 
 
-  async updateFile(req,res) {
-    // Add file path to request body
-    if (req.file) req.body["fileURL"] = req.file.path
+    async deleteFile(req, res) {
+        const file = await File.deleteOne({ _id: req.params.id }, (err, file) => {
+            if (err) throw new CustomError("Error occured while deleting file");
+            if (!file)
+                return res.status(404).json(response("File Not Found", null, false));
 
-    const file = await File.findOneAndUpdate(
-        {_id: req.params.fileId}, 
-        req.body, 
-        {new: true}, 
-        (err,file) => {
-          if(err) throw new CustomError("Error: File could not be updated");
+            res.status(200).json(response("File Deleted", null, true));
+        });
+    }
 
-          if(!file)
-          return res.status(404).json(response("File with ID not found", null, false));
 
-          res.status(200).json(response("File updated successfully", file, true));
-      })
-  }
+    async updateFile(req, res) {
+        // Add file path to request body
+        if (req.file) req.body["fileURL"] = req.file.path
+
+        const file = await File.findOneAndUpdate(
+            { _id: req.params.fileId },
+            req.body,
+            { new: true },
+            (err, file) => {
+                if (err) throw new CustomError("Error: File could not be updated");
+
+                if (!file)
+                    return res.status(404).json(response("File with ID not found", null, false));
+
+                res.status(200).json(response("File updated successfully", file, true));
+            })
+    }
 }
 
 module.exports = new FileContoller();
