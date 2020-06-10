59d449183bbefe9182f09b7ce6e659089542d69d
I F E DI L I ⚡ <OnyegbuIfedili@gmail.com>
2020-06-10 03:38:15 +0100

    Develop (#7)
    * Develop (#17)
    
    * add async handler function
    
    * feat: api to get single file
    
    * Remove.js: worked with model (#3)
    
    Co-authored-by: Solomon Ajayi <temmyjay001@gmail.com>
    
    * created the getFiles route and handler (#4)
    
    * Pulled
    
    * feat: add response interface for requests
    
    * feat: add an api to get a single files
    
    * feat: add error handler middleware
    
    * feat: added custom error
    
    * fix: fixed controller bug
    
    * fix: controller getfiles()
    
    * fix: file controller bugs
    
    * Develop (#2)
    
    * Remove.js: worked with model
    
    * Pulled
    
    Co-authored-by: Solomon Ajayi <temmyjay001@gmail.com>
    
    * async/await for deleteFile
    
    * Update README.md
    
    * remodification of deleteFile
    
    * made changes to path
    
    * Develop (#16)
    
    * Revert "Remove.js: worked with model"
    
    * feat: multer integration
    
    * Develop (#4)
    
    * async/await for deleteFile
    
    * Update README.md
    
    * remodification of deleteFile
    
    * made changes to path
    
    Co-authored-by: Solomon Ajayi <temmyjay001@gmail.com>
    
    Co-authored-by: Samuel Onyijne <samuelonyijne@gmail.com>
    Co-authored-by: Samuel Onyijne <samuelord1@gmail.com>
    Co-authored-by: Solomon Ajayi <temmyjay001@gmail.com>
    
    Co-authored-by: stefanpgr <stefanpongrz@gmail.com>
    Co-authored-by: Alao-Abiodun <abiodunDev@gmail.com>
    Co-authored-by: Solomon Ajayi <temmyjay001@gmail.com>
    Co-authored-by: Chia Gabriel <chiagabrielson@gmail.com>
    Co-authored-by: Samuel Onyijne <samuelonyijne@gmail.com>
    Co-authored-by: Samuel Onyijne <samuelord1@gmail.com>
    
    * update route
    
    * fixed a bug
    
    * fixed a bug
    
    * fixed a bug
    
    * better configuration to delete endpoint and multer
    
    * Create test and tested it, Made changes to the update endpoint
    
    Co-authored-by: stefanpgr <stefanpongrz@gmail.com>
    Co-authored-by: Alao-Abiodun <abiodunDev@gmail.com>
    Co-authored-by: Solomon Ajayi <temmyjay001@gmail.com>
    Co-authored-by: Chia Gabriel <chiagabrielson@gmail.com>
    Co-authored-by: Samuel Onyijne <samuelonyijne@gmail.com>
    Co-authored-by: Samuel Onyijne <samuelord1@gmail.com>
    Co-authored-by: olucheye <seyefree2rhyme@yahoo.com>
    Co-authored-by: ajibadeabd <ajibadeabd@gmail.com>

--- controllers/files.js
+++ controllers/files.js
@@ -1,10 +1,10 @@
 const File = require("./../models/file.model");
 const response = require("./../utils/response");
 const CustomError = require("./../utils/CustomError");
-
+const fs = require("fs");
 /**
  * Controllers for :
- * 
+ *
  * getFiles
  * getFile,
  * createFile,
@@ -14,13 +14,14 @@ const CustomError = require("./../utils/CustomError");
 
 class FileContoller {
   // Add file
-  async createFile(req,res){
-    if (req.file) req.body["fileURL"] = req.file.path
+  async createFile(req, res) {
+    // Add file path to request body
+    if (req.file) req.body["fileURL"] = req.file.path;
 
-    let file = new File(req.body)
-    file.save()
+    let file = new File(req.body);
+    file.save();
 
-    res.status(201).json(response("File created", file, true))
+    res.status(201).json(response("File created", file, true));
   }
 
   // Get one file
@@ -31,8 +32,8 @@ class FileContoller {
       if (!file)
         return res.status(404).json(response("File Not Found", null, false));
 
-      res.status(200).json(response("File Found", file, true))
-    })
+      res.status(200).json(response("File Found", file, true));
+    });
   }
 
   //route handler to get all files
@@ -46,12 +47,53 @@ class FileContoller {
   }
 
   async deleteFile(req, res) {
-    const file = await File.deleteOne({ _id: req.params.id }, (err, file) => {
-      if (err) throw new CustomError("Error occured while deleting file");
+    await File.findOne({ _id: req.params.fileId }, (err, file) => {
+      const filename = file.fileURL.split("uploads/")[1];
+      fs.unlink(`uploads/${filename}`, async () => {
+        await File.deleteOne({ _id: req.params.id }, (err, file) => {
+          if (err) throw new CustomError("Error occured while deleting file");
+          if (!file)
+            return res
+              .status(404)
+              .json(response("File Not Found", null, false));
+
+          res.status(200).json(response("File Deleted", null, true));
+        });
+      });
+    });
+  }
+
+  async updateFile(req, res) {
+    // Add file path to request body
+    if (req.file) req.body["fileURL"] = req.file.path;
+
+    await File.findOne({ _id: req.params.fileId }, (err, file) => {
+      const filename = file.fileURL.split("uploads/")[1];
+      fs.unlink(`uploads/${filename}`, async () => {
+        await File.findOneAndUpdate(
+          {
+            _id: req.params.fileId,
+          },
+          req.body,
+          { new: true },
+          (err, file) => {
+            if (err) throw new CustomError("Error: File could not be updated");
+
+            if (!file)
+              return res
+                .status(404)
+                .json(response("File with ID not found", null, false));
+
+            res
+              .status(200)
+              .json(response("File updated successfully", file, true));
+          }
+        );
+      });
+      if (err) throw new CustomError("Error occured while retriving files");
+
       if (!file)
         return res.status(404).json(response("File Not Found", null, false));
-
-      res.status(200).json(response("File Deleted", null, true));
     });
   }
 }
--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-require('express-async-errors')
-const express = require('express');
-const cors = require('cors');
+require("express-async-errors");
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const morgan = require("morgan")
-const path = require("path")
+const morgan = require("morgan");
+const path = require("path");
 
-const port = process.env.PORT;
-const fileRoutes = require("./routes/files")
-const errorMiddleware = require("./middleware/error")
-const initDB = require("./config/db")
+const port = process.env.PORT || 3000;
+const fileRoutes = require("./routes/files");
+const errorMiddleware = require("./middleware/error");
+const initDB = require("./config/db");
 
-app.use(cors())
-app.use(morgan('dev'));
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, './uploads/')));
+app.use("/uploads", express.static(path.join(__dirname, "./uploads/")));
 
 //Api routes
-app.use('/api/v1/files', fileRoutes);
+app.use("/api/v1/files", fileRoutes);
 
 //Handle invalid api endpoints
 app.use((req, res, next) => {
-     throw new CustomError("Invalid request", 400);
+  throw new CustomError("Invalid request", 400);
 });
 
 // Handle server erros
@@ -29,10 +29,12 @@ app.use(errorMiddleware);
 
 // Listen to port
 app.listen(port, () => {
-     console.log(`::: Server listening on port ${port}`);
-     initDB()
+  console.log(`::: Server listening on port ${port}`);
+  initDB();
 });
 
-app.on('error', (error) => {
-     console.log("::> An error occiurred in our server " + error);
+app.on("error", (error) => {
+  console.log("::> An error occiurred in our server " + error);
 });
+
+module.exports = app;
